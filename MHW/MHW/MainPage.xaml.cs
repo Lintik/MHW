bc452ff9c2d5c10979bc60fd9aa9a1fcac1527b9
Lintik <domandominguez@hotmail.com>
2018-04-28 13:20:11 -0700

    <add Equipment Builder>

--- MHW/MHW/MainPage.xaml.cs
+++ MHW/MHW/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using SQLite;
+
 namespace MHW
 {
 	public partial class MainPage : ContentPage
@@ -18,5 +20,21 @@ namespace MHW
 		{
 		    Navigation.PushAsync(new Compendium());
 		}
-	}
+
+        private void SearchSelected(object Sender, EventArgs e)
+        {
+            using (var conn = new SQLiteConnection(App.DBPath))
+            {
+                conn.CreateTable<Armor>();
+                var equip = conn.Query<Armor>("select * from Armor where lower(name) like ?", "%" + (EquipmentKeyword.Text).ToLower() + "%").ToList();
+                if(equip[0].name != null)
+                    ToBeEquip.Text = equip[0].name;
+            }
+        }
+        private void EquipSelected(object Sender, EventArgs e)
+        {
+
+        }
+
+    }
 }
