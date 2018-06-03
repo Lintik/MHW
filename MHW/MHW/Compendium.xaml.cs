dae384e65f42712b689c9ad595ee450afb0f937b
Marco Domniguez <DomanDominguez@hotmail.com>
2018-06-02 20:21:16 -0700

    <update db>

--- MHW/MHW/Compendium.xaml.cs
+++ MHW/MHW/Compendium.xaml.cs
@@ -21,6 +21,7 @@ namespace MHW
             InitializeComponent();
         }
 
+        //Search database on button pressed
         private void SearchMHWDB(object sender, EventArgs e)
         {
             var keyword = MainSearchBar.Text;
@@ -29,13 +30,31 @@ namespace MHW
             {
                 conn.CreateTable<Armor>();
 
-                var equip = conn.Query<Armor>("select * from Armor where lower(name) like ? UNION select * from Armor where lower(att0) like ? UNION select * from Armor where lower(att1) like ? order by id desc",
+                var equip = conn.Query<Armor>("select * from Armor where lower(name) like ? UNION select * from Armor where lower(att1) like ? UNION select * from Armor where lower(att2) like ? order by id desc",
                    "%" + keyword.ToLower() + "%",
                    "%" + keyword.ToLower() + "%",
                    "%" + keyword.ToLower() + "%").ToList();
                 MHWDBListView.ItemsSource = equip;
             }
         }
+
+        //Search database on text changed
+        private void TextChangedMHWDB(object sender, EventArgs e)
+        {
+            var keyword = MainSearchBar.Text;
+
+            using (var conn = new SQLiteConnection(App.DBPath))
+            {
+                conn.CreateTable<Armor>();
+
+                var equip = conn.Query<Armor>("select * from Armor where lower(name) like ? UNION select * from Armor where lower(att1) like ? UNION select * from Armor where lower(att2) like ? order by id desc",
+                   "%" + keyword.ToLower() + "%",
+                   "%" + keyword.ToLower() + "%",
+                   "%" + keyword.ToLower() + "%").ToList();
+                MHWDBListView.ItemsSource = equip;
+            }
+        }
+
 
 
         protected override void OnAppearing()
