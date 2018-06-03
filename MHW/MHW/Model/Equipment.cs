7d17dbc895f792615960f9249b610b55f1616787
Marco Domniguez <DomanDominguez@hotmail.com>
2018-06-03 14:50:08 -0700

    <add Equipment.cs>

--- MHW/MHW/Model/Equipment.cs
+++ MHW/MHW/Model/Equipment.cs
@@ -7,5 +7,9 @@ namespace MHW.Model
     public class Equipment
     {
         public static Dictionary<string, int> EquippedSkills;
+        public static int Slot1Count = 0;
+        public static int Slot2Count = 0;
+        public static int Slot3COunt = 0;
+        public static List<int> EquippedID;
     }
 }
