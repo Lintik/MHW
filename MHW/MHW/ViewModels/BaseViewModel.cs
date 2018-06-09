1c441a9fe0d7b0d33c80767d609db56b81f912e4
Marco Domniguez <DomanDominguez@hotmail.com>
2018-06-09 15:58:52 -0700

    <update icons>

---
+++ MHW/MHW/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MHW.ViewModels
+{
+    public class BuilderViewModel :INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+        #endregion 
+    }
+}
